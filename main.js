48f1597d031c3a164097bc16d2ec63dee9b087d9
Waly <ahmadouwalyndiaye@gmail.com>
2021-11-01 14:29:15 +0000

    - Mocked the endpoint - Handle variant section content from js

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
 import "./style.css";
 import "./components/variant";
+import response from "./data";
 
+function $(selector) {
+  return document.querySelector(selector);
+}
+function createNode(element) {
+  return document.createElement(element);
+}
+function append(parent, el) {
+  return parent.appendChild(el);
+}
 window.addEventListener("DOMContentLoaded", async () => {
-  try {
-    const response = await fetch(
-      "https://api.clever-cloud.com/v2/products/instance"
-    );
-    if (response.ok) {
-      const variant = await response.json();
-      console.log("variant ==>", variant);
-    } else {
-      throw response;
-    }
-  } catch (error) {
-    console.log("error ==>", error);
-  }
+  const $ul = $("#variants");
+  response.forEach((obj) => {
+    const variant = obj.variant;
+    const $li = createNode("li");
+    const $ccVariant = createNode("cc-variant");
+    $ccVariant.setAttribute("variant", JSON.stringify(variant));
+    append($li, $ccVariant);
+    append($ul, $li);
+  });
+  // try {
+  //   const response = await fetch(
+  //     "https://api.clever-cloud.com/v2/products/instance"
+  //   );
+  //   if (response.ok) {
+  //     const variant = await response.json();
+  //     console.log("variant ==>", variant);
+  //   } else {
+  //     throw response;
+  //   }
+  // } catch (error) {
+  //   console.log("error ==>", error);
+  // }
 });
