938f984664f2134abc014a91f90d687c42b172ad
Waly <ahmadouwalyndiaye@gmail.com>
2021-11-01 12:26:18 +0000

    [wip] Integration endpoint to fetch variants

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-import './style.css'
-import './components/variant';
+import "./style.css";
+import "./components/variant";
+
+window.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const response = await fetch(
+      "https://api.clever-cloud.com/v2/products/instance"
+    );
+    if (response.ok) {
+      const variant = await response.json();
+      console.log("variant ==>", variant);
+    } else {
+      throw response;
+    }
+  } catch (error) {
+    console.log("error ==>", error);
+  }
+});
